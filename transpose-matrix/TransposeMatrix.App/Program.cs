02a4f54dd1097d1178425801b18be495633c60c4
patrikduch <duchpatrik@icloud.com>
2023-09-23 10:59:54 +0200

    feat:  TransposeMatrix algorithm

---
+++ transpose-matrix/TransposeMatrix.App/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TransposeMatrix.App;
+
+Console.WriteLine("Transpose2DMatrix Algorithm");
+
+var array2D = new int[2, 2];
+array2D[0, 0] = 1;
+array2D[0, 1] = 2;
+array2D[1, 0] = 3;
+array2D[1, 1] = 4;
+
+
+Console.WriteLine("Before transpose");
+
+for (int i = 0; i < array2D.GetLength(0); i++)
+{
+    for (int j = 0; j < array2D.GetLength(1); j++)
+    {
+        Console.Write(array2D[i, j] + "\t");  // Display element and add a tab space
+    }
+    
+    Console.WriteLine();  // New line after each row
+}
+
+var algorithm = new TransposeMatrixAlgorithm();
+
+array2D = algorithm.TransposeMatrix(array2D);
+
+
+Console.WriteLine("After transpose");
+// Display the 2D array as a matrix
+for (int i = 0; i < array2D.GetLength(0); i++)  // Iterate over rows
+{
+    for (int j = 0; j < array2D.GetLength(1); j++)  // Iterate over columns
+    {
+        Console.Write(array2D[i, j] + "\t");  // Display element and add a tab space
+    }
+    Console.WriteLine();  // New line after each row
+}
---
+++ transpose-matrix/TransposeMatrix.App/TransposeMatrixAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿namespace TransposeMatrix.App;
+
+public class TransposeMatrixAlgorithm
+{
+    public int[,] TransposeMatrix(int[,] matrix)
+    {
+        var rows = matrix.GetLength(0);
+        var columns = matrix.GetLength(1);
+
+        var transposeMatrix = new int[columns, rows];
+        
+        for (var i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < matrix.GetLength(1); j++)
+            {
+                transposeMatrix[j, i] = matrix[i,j];
+            }
+        }
+
+
+        return transposeMatrix;
+    }
+}

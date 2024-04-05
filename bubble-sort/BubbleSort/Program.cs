62608d9d16bc585f3965497e3a79c29254f2b9b5
patrikduch <duchpatrik@icloud.com>
2024-04-05 15:41:03 +0200

    bubble sort impl

---
+++ bubble-sort/BubbleSort/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BubbleSort;
+
+Console.WriteLine("Hello, World!");
+
+
+var test = TaskAssignment.BubbleSort(new int[] {1,5,2});
+
+foreach (var element in test)
+{
+    
+    Console.WriteLine(element);
+}
---
+++ bubble-sort/BubbleSort/TaskAssignment.cs
@@ -0,0 +1,23 @@
+﻿namespace BubbleSort;
+
+public class TaskAssignment
+{
+    public static int[] BubbleSort(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i; j < array.Length-1; j++)
+            {
+                var tmp = array[j];
+
+                // Order of swapping
+                if (array[j] < array[j+1])
+                {
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+}

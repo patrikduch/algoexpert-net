8ab14281c52b22debd46443c0c072655372d9b52
patrikduch <duchpatrik@icloud.com>
2024-04-05 12:49:31 +0200

    TwoNumberSum impl

---
+++ two-number-sum/TwoNumberSum/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TwoNumberSum;
+
+var result = TaskAssignment.TwoNumberSum(new[] {1,1}, 2);
+
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+ 
---
+++ two-number-sum/TwoNumberSum/TaskAssignment.cs
@@ -0,0 +1,25 @@
+﻿namespace TwoNumberSum;
+
+public class TaskAssignment
+{
+
+    public static int[] TwoNumberSum(int[] array, int targetSum)
+    {
+        Dictionary<int, bool> dict = new Dictionary<int, bool>();
+
+        foreach (var item in array)
+        {
+            var potentialMatch = targetSum - item;
+
+            if (dict.ContainsKey(potentialMatch))
+            {
+                return new[] { item, potentialMatch };
+            }
+
+            dict[potentialMatch] = true;
+
+        }
+
+        return new int[] { };
+    }
+}
